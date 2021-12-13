deaa9ae82f80b90ebd957dc783e15e12684535a2
gabriel <gabriel.pr07@hotmail.com>
2021-12-12 22:32:19 -0300

    chore: add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:16
+WORKDIR /usr/src/usefashionstore/backend
+COPY ./package.json .
+RUN npm install --only=prod
+COPY ./dist ./dist
+EXPOSE 8080
+CMD npm start
--- src/data/useCases/product/addProduct/dbAddProduct.ts
+++ src/data/useCases/product/addProduct/dbAddProduct.ts
@@ -1,6 +1,14 @@
 import { AddProductRepository } from '../../../../data/protocols/db/product/addProductRepository'
 import { AddProduct, AddProductParams } from '../../../../domain/useCases/product/addProduct'
 
+/*
+  my use case class, or my domain class,
+  must not point to or implement a class linked to the infra,
+  it's infra that must point to the domain classes
+  AddProductRepository is a abstraction
+
+  apontar = depender/dependência ou ate mesmo implementar
+*/
 export class DbAddProduct implements AddProduct {
   private readonly addProductRepository: AddProductRepository
   constructor (addProductRepository: AddProductRepository) {
