58d509c925043d55dc5e2b41663c63df6fc91137
gabriel <gabriel.pr07@hotmail.com>
2021-12-13 00:49:43 -0300

    chore: add docker-compose

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,3 @@ FROM node:16
 WORKDIR /usr/src/usefashionstore/backend
 COPY ./package.json .
 RUN npm install --only=prod
-COPY ./dist ./dist
-EXPOSE 8080
-CMD npm start
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/usefashionstorelook',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/usefashionstorelook',
   port: process.env.PORT || 8081,
   jwtSecret: process.env.JWT_SECRET || 'gunfh4430feoj23ddr2rwgwwt2'
 }
